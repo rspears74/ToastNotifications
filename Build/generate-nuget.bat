a87fc60f76871811a4b5c87177f89b745fa3bc52
Rafał Łopatka <alzur@outlook.com>
2017-03-21 19:42:34 +0100

    nuget

--- Build/generate-nuget.bat
+++ Build/generate-nuget.bat
@@ -1,1 +1,2 @@
-nuget.exe pack ToastNotifications.csproj -Prop Configuration=Release
+nuget.exe pack ../Src/ToastNotifications/ToastNotifications.csproj -Prop Configuration=Release
+nuget.exe pack ../Src/ToastNotifications.Messages/ToastNotifications.Messages.csproj -Prop Configuration=Release
